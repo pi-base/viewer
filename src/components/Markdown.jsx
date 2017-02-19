66c0f14808885f71975d8aad70cf3c948eabc013
James Dabbs <jamesdabbs@gmail.com>
2017-02-19 11:31:00 -0500

    Change markdown providers
    Showdown disables midword_em_tags, which often show up in Tex

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import marked from 'marked'
+
+import showdown from 'showdown'
+const converter = new showdown.Converter()
+converter.setFlavor('github')
 
 const rawMarkup = (text) => {
-    return { __html: marked(text, {sanitize: true}) }
+  return { __html: converter.makeHtml(text) }
 }
 
 const Markdown = ({ text }) => {
-    if (text) {
-        return <div className="markdown" dangerouslySetInnerHTML={rawMarkup(text)}/>
-    } else {
-        return <div className="markdown"/>
-    }
+  if (text) {
+    return <div className="markdown" dangerouslySetInnerHTML={rawMarkup(text)}/>
+  } else {
+    return <div className="markdown"/>
+  }
 }
 
 export default Markdown
