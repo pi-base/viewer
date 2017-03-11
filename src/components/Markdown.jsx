11c7f2adc83195320ed04b4a14d2f55b18d40d13
James Dabbs <jamesdabbs@gmail.com>
2017-03-11 17:58:21 -0500

    Extract and improve markdown generation. Fixes #16

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,40 +1,15 @@
 import React from 'react'
 
-import showdown from 'showdown'
-
-const escapeTeX = () => {
-  const escape = (match) => {
-    // Escape any markdown special characters
-    return match.replace(/([\\`*_{}\[\]()#+-.!])/g, '\\$1')
-  }
-
-  return [
-    {
-      type: 'lang',
-      regex: /\\\((.*?)\\\)/g, // Inside TeX delimiters
-      replace: (_, s) => ('\\(' + escape(s) + '\\)')
-    },
-    {
-      type: 'lang',
-      regex: /¨D(.*?)¨D/g, // Showdown converts $ internally into ¨D
-      replace: (_, s) => ('$' + escape(s) + '$')
-    }
-  ]
-}
-
-const converter = new showdown.Converter({extensions: [escapeTeX]})
-// converter.setFlavor('github')
+import { markdown } from 'pi-base-core'
 
 const rawMarkup = (text) => {
-  return { __html: converter.makeHtml(text) }
+  return { __html: markdown(text) }
 }
 
 const Markdown = ({ text }) => {
-  if (text) {
-    return <div className="markdown" dangerouslySetInnerHTML={rawMarkup(text)}/>
-  } else {
-    return <div className="markdown"/>
-  }
+  if (!text) { return <div className="markdown"/> }
+
+  return <div className="markdown" dangerouslySetInnerHTML={rawMarkup(text)}/>
 }
 
 export default Markdown
