0fea7288970efc7f5186a793d66c561a47447adb
Steven Clontz <steven.clontz@gmail.com>
2017-12-11 12:14:53 -0600

    cache refresh in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
+let refresh = (e) => {
+  e.preventDefault();
+  localStorage.clear();
+  location.reload();
+}
+
 const Footer = () => (
   <div className="row">
     <div className="col-xs-12">
       <hr/>
-      Having trouble? Try <a href="#">refreshing your cache</a>.
+      Having trouble? Try{' '}
+      <a
+        href="#"
+        onClick={refresh}>
+        refreshing your cache
+      </a>.
     </div>
   </div>
 )
