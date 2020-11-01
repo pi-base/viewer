4a511cef97f28b1c7835c09e49a66dd1b4925b9f
James Dabbs <jamesdabbs@gmail.com>
2020-11-01 06:32:40 -0800

    Add initial space pages
    The list is mostly a placeholder for now

---
+++ src/components/Spaces/List.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { spaces } from '../../context'
+  import { list } from '../../stores'
+
+  import { Filter, Id, Link, Title, Typeset } from '../Shared'
+
+  const index = list(spaces(), {
+    weights: { name: 0.7, aliases: 0.7, description: 0.3 },
+    queryParam: 'filter',
+  })
+</script>
+
+<Title title="Spaces" />
+
+<Filter filter={index.filter} />
+
+<table class="table">
+  <thead>
+    <tr>
+      <th on:click={index.sort('uid')}>Id</th>
+      <th on:click={index.sort('name')}>Name</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  {#each $index as { uid, name, description } (uid)}
+    <tr>
+      <td>
+        <Link to="/spaces/{uid}">
+          <Id {uid} />
+        </Link>
+      </td>
+      <td>
+        <Typeset body={name} />
+      </td>
+      <td>
+        <Typeset body={description} truncated={true} />
+      </td>
+    </tr>
+  {/each}
+</table>
---
+++ src/components/Spaces/Show.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { spaces } from '../../context'
+  import { Aliases, References, Tabs, Title, Typeset } from '../Shared'
+
+  export let id: string
+
+  const store = spaces()
+  $: space = $store.find(id)
+</script>
+
+{#if space}
+  <Title title={space.name} />
+
+  <h1>
+    <Typeset body={space.name} />
+    {#if space?.aliases}
+      <Aliases aliases={space.aliases} />
+    {/if}
+  </h1>
+
+  <Typeset body={space.description} />
+
+  <Tabs.Tabs initial="references">
+    <Tabs.Nav>
+      <Tabs.Link to="references">References</Tabs.Link>
+    </Tabs.Nav>
+
+    <Tabs.Tab path="references">
+      <References references={space.refs} />
+    </Tabs.Tab>
+  </Tabs.Tabs>
+{/if}
