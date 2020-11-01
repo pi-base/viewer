c365a74e0f5c4e5e60e12428f592e5122155a4af
James Dabbs <jamesdabbs@gmail.com>
2020-11-01 07:06:51 -0800

    Add related spaces / properties tabs

--- src/components/Shared/Icons/index.ts
+++ src/components/Shared/Icons/index.ts
@@ -1,1 +1,5 @@
+// TODO: rework this into a base Icon type with data values
+export { default as Check } from './Check.svelte'
 export { default as Repeat } from './Repeat.svelte'
+export { default as Search } from './Search.svelte'
+export { default as X } from './X.svelte'
--- src/components/Spaces/Show.svelte
+++ src/components/Spaces/Show.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { spaces } from '../../context'
   import { Aliases, References, Tabs, Title, Typeset } from '../Shared'
+  import Properties from './Properties.svelte'
 
   export let id: string
 
@@ -20,11 +21,15 @@
 
   <Typeset body={space.description} />
 
-  <Tabs.Tabs initial="references">
+  <Tabs.Tabs initial="properties">
     <Tabs.Nav>
+      <Tabs.Link to="properties">Properties</Tabs.Link>
       <Tabs.Link to="references">References</Tabs.Link>
     </Tabs.Nav>
 
+    <Tabs.Tab path="properties">
+      <Properties {space} />
+    </Tabs.Tab>
     <Tabs.Tab path="references">
       <References references={space.refs} />
     </Tabs.Tab>
