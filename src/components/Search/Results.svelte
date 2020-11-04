237041f42bdf6216530d0a93f20eaea5090de17c
James Dabbs <jamesdabbs@gmail.com>
2020-11-03 19:32:54 -0800

    Render search results

--- src/components/Search/Results.svelte
+++ src/components/Search/Results.svelte
@@ -1,14 +1,59 @@
 <script lang="ts">
+  import * as F from '@pi-base/core/lib/Formula'
+
+  import { Formula, Link, Typeset } from '../Shared'
+  import { Value } from '../Traits'
   import context from '../../context'
   import { search } from '../../stores'
-  import type { Formula, Property } from '../../types'
+  import type { Property, Space } from '../../types'
 
   export let text: string
-  export let formula: Formula<Property> | undefined
+  export let formula: F.Formula<Property> | undefined
 
-  const store = search(context())
+  const { spaces, traits } = context()
+  const store = search({ spaces, traits })
 
   $: results = store.search({ text, formula })
+  $: properties = formula ? Array.from(F.properties(formula)) : []
 </script>
 
-<pre>{JSON.stringify({ text, formula, results }, null, 2)}</pre>
+Spaces
+{#if text}matching <code>{text}</code>{/if}
+{#if text && formula}and{/if}
+{#if formula}
+  satisfying
+  <Formula value={formula} />
+{/if}
+
+<table class="table">
+  <thead>
+    <tr>
+      <td />
+      {#each properties as { uid, name: property }}
+        <td>
+          <Link to="/properties/${uid}">
+            <Typeset body={property} />
+          </Link>
+        </td>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each results as space (space.uid)}
+      <tr>
+        <td>
+          <Link to="/spaces/${space.uid}">
+            <Typeset body={space.name} />
+          </Link>
+        </td>
+        {#each properties as property (property.uid)}
+          <td>
+            <Link to="/spaces/{space.uid}/properties/{property.uid}">
+              <Value value={$traits.find(space, property)?.value} />
+            </Link>
+          </td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
--- src/components/Shared/Icons/index.ts
+++ src/components/Shared/Icons/index.ts
@@ -1,5 +1,6 @@
 // TODO: rework this into a base Icon type with data values
 export { default as Check } from './Check.svelte'
+export { default as Question } from './Question.svelte'
 export { default as Repeat } from './Repeat.svelte'
 export { default as Search } from './Search.svelte'
 export { default as X } from './X.svelte'
