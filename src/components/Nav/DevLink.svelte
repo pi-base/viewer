625aa54ba6c413ee484b21816f2a7b2ee485c8a3
James Dabbs <james.dabbs@gmail.com>
2021-09-27 19:00:36 -0700

    Port several misc. views to Svelte

---
+++ src/components/Nav/DevLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { MAIN_BRANCH } from '../../models/Store'
+
+  export let branch: string
+</script>
+
+<a class="nav-link" href="/dev">
+  {#if branch === MAIN_BRANCH}
+    Dev
+  {:else}
+    <!-- TODO: branch icon -->
+    {branch}
+  {/if}
+</a>
