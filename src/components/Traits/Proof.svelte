6f9c39a387980f1d0b53df41255e0da2100be92d
James Dabbs <james.dabbs@gmail.com>
2021-09-27 18:24:20 -0700

    Port Trait views to Svelte

---
+++ src/components/Traits/Proof.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { Space } from '../../models'
+  import { Proof, default as S } from '../../models/Store'
+  import Theorems from '../Theorems/SummaryList.svelte'
+  import Traits from '../Spaces/TraitsForProperties.svelte'
+  import { getStore } from '../Svelte'
+
+  export let space: Space
+  export let proof: Proof
+
+  const store = getStore()
+
+  $: theorems = proof.theorems.map((id: string) => S.theorem(store, id)!)
+  $: properties = proof.properties.map((id: string) => S.property(store, id)!)
+</script>
+
+<p>Automatically deduced from the following</p>
+<div class="row">
+  <div class="col-6">
+    <h5>Properties</h5>
+    <Traits {space} {properties} />
+  </div>
+  <div class="col-6">
+    <h5>Theorems</h5>
+    <Theorems {theorems} />
+  </div>
+</div>
--- src/components/Traits/index.tsx
+++ src/components/Traits/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-import Trait from './Detail'
+import Detail from './Detail.svelte'
 import { Store, default as S } from '../../models/Store'
 import RouteLookup from '../Shared/RouteLookup'
+import { Svelte } from '../Svelte'
 
-const findTrait = (store: Store, { spaceId, propertyId }: { spaceId: string, propertyId: string }) => {
-  if (!spaceId || !propertyId) { return }
+const findTrait = (
+  store: Store,
+  { spaceId, propertyId }: { spaceId: string; propertyId: string }
+) => {
+  if (!spaceId || !propertyId) {
+    return
+  }
 
   const space = S.space(store, spaceId)
   const property = S.property(store, propertyId)
@@ -14,12 +20,10 @@ const findTrait = (store: Store, { spaceId, propertyId }: { spaceId: string, pro
   return property && space && trait && { property, space, trait }
 }
 
+export function Trait(props: any) {
+  return <Svelte component={Detail} props={props} />
+}
+
 export default function Traits({ path }: { path: string }) {
-  return (
-    <RouteLookup
-      path={path}
-      lookup={findTrait}
-      component={Trait}
-    />
-  )
+  return <RouteLookup path={path} lookup={findTrait} component={Trait} />
 }
